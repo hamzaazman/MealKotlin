5d34ba26bce086fa08c0199e45296f0230833e1f
Hamza Azman <hamzaazman@outlook.com>
2022-09-06 00:48:54 +0300

    Create new fragments

---
+++ app/src/main/java/com/example/mealkotlin/ui/DetailFragment.kt
@@ -0,0 +1,22 @@
+package com.example.mealkotlin.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mealkotlin.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+}
