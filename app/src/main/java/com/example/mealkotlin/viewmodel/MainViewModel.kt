ab16b80bdcb2238a000eccbeb4bc21ec14130139
Hamza Azman <hamzaazman@outlook.com>
2022-09-07 14:40:43 +0300

    Fetch meals to by category name

--- app/src/main/java/com/example/mealkotlin/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mealkotlin/viewmodel/MainViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.mealkotlin.data.Repository
 import com.example.mealkotlin.models.Categories
+import com.example.mealkotlin.models.Meals
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import java.lang.Exception
@@ -19,6 +20,7 @@ class MainViewModel @Inject constructor(
 ) : AndroidViewModel(application) {
 
     var categoryResult: MutableLiveData<Categories> = MutableLiveData()
+    var mealsResult: MutableLiveData<Meals> = MutableLiveData()
 
     fun getCategories() = viewModelScope.launch {
         try {
@@ -28,6 +30,19 @@ class MainViewModel @Inject constructor(
             }
         } catch (e: Exception) {
             Log.e("getCategories Error : ", e.message.toString())
+        }
+    }
+
+    fun getMealsByCategoryName(categoryName: String) = viewModelScope.launch {
+        try {
+            val response = repository.remote.getMealsByCategoryName(categoryName)
+            if (response.isSuccessful) {
+                mealsResult.value = response.body()
+            } else {
+                Log.e("getMealViewModel: ", response.errorBody().toString())
+            }
+        } catch (e: Exception) {
+            Log.e("getMealByCategoryName: ", e.message.toString())
         }
     }
 
