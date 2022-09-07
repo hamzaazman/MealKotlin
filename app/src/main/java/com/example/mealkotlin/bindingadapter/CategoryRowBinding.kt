0eab839509fc065a0dfd654ab4361780bdf6e230
Hamza Azman <hamzaazman@outlook.com>
2022-09-07 14:41:39 +0300

    Add click listener to category card

--- app/src/main/java/com/example/mealkotlin/bindingadapter/CategoryRowBinding.kt
+++ app/src/main/java/com/example/mealkotlin/bindingadapter/CategoryRowBinding.kt
@@ -1,22 +1,40 @@
 package com.example.mealkotlin.bindingadapter
 
 import android.widget.ImageView
-import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
+import androidx.navigation.findNavController
+import com.example.mealkotlin.R
+import com.example.mealkotlin.ui.CategoriesFragmentDirections
+import com.squareup.picasso.Picasso
 
-class CategoryRowBinding {
+object CategoryRowBinding {
 
-    companion object {
 
-        @BindingAdapter("loadCategoryImageView")
-        @JvmStatic
-        fun loadCategoryImageView(imageView: ImageView, imageUrl: String) {
-            Glide
-                .with(imageView.context)
-                .load(imageUrl)
-                .into(imageView)
-        }
+    @BindingAdapter("loadCategoryImageView")
+    @JvmStatic
+    fun loadCategoryImageView(imageView: ImageView, imageUrl: String) {
+        /*Glide
+            .with(imageView.context)
+            .load(imageUrl)
+            .into(imageView)*/
 
+        Picasso.get()
+            .load(imageUrl)
+            .placeholder(R.drawable.placeholder_image)
+            .into(imageView)
     }
+
+    @BindingAdapter("onClickCategoryListener")
+    @JvmStatic
+    fun onClickCategoryListener(categoryRowLayout: CardView, categoryName: String) {
+        categoryRowLayout.setOnClickListener {
+            val action =
+                CategoriesFragmentDirections.actionCategoriesFragmentToMealsFragment(
+                    categoryName
+                )
+            categoryRowLayout.findNavController().navigate(action)
+        }
+    }
+
 }
