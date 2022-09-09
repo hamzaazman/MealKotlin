e0b2a93331dcec492d34ce6f7dfceb81d4b56be9
Hamza Azman <hamzaazman@outlook.com>
2022-09-09 16:13:32 +0300

    Add filter functions for fetch meals and search meals

--- app/src/main/java/com/example/mealkotlin/data/network/FoodApi.kt
+++ app/src/main/java/com/example/mealkotlin/data/network/FoodApi.kt
@@ -1,12 +1,14 @@
 package com.example.mealkotlin.data.network
 
 import com.example.mealkotlin.models.Categories
+import com.example.mealkotlin.models.Ingredient
+import com.example.mealkotlin.models.Meal
 import com.example.mealkotlin.models.Meals
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface FoodApi{
+interface FoodApi {
 
     @GET("categories.php")
     suspend fun getCategories(): Response<Categories>
@@ -17,4 +19,13 @@ interface FoodApi{
         categoryName: String
     ): Response<Meals>
 
+    @GET("lookup.php")
+    suspend fun getMealIngredientByMealId(
+        @Query("i") mealId: Int
+    ): Response<Ingredient>
+
+    @GET("search.php")
+    suspend fun getFilterByMeals(
+        @Query("s") searchText: String
+    ): Response<Meals>
 }
--- app/src/main/java/com/example/mealkotlin/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mealkotlin/viewmodel/MainViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.mealkotlin.data.Repository
 import com.example.mealkotlin.models.Categories
+import com.example.mealkotlin.models.Ingredient
 import com.example.mealkotlin.models.Meals
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -21,6 +22,8 @@ class MainViewModel @Inject constructor(
 
     var categoryResult: MutableLiveData<Categories> = MutableLiveData()
     var mealsResult: MutableLiveData<Meals> = MutableLiveData()
+    var mealIngredient: MutableLiveData<Ingredient> = MutableLiveData()
+    var searchByMeals: MutableLiveData<Meals> = MutableLiveData()
 
     fun getCategories() = viewModelScope.launch {
         try {
@@ -43,6 +46,29 @@ class MainViewModel @Inject constructor(
             }
         } catch (e: Exception) {
             Log.e("getMealByCategoryName: ", e.message.toString())
+        }
+    }
+
+    fun getIngredientByMealId(mealId: Int) = viewModelScope.launch {
+        try {
+            val response = repository.remote.getIngredientMealByMealId(mealId)
+            Log.d("Meal Id", response.body().toString())
+            if (response.isSuccessful) {
+                mealIngredient.value = response.body()
+            }
+        } catch (e: Exception) {
+            Log.e("Error Ingredient", e.message.toString())
+        }
+    }
+
+    fun getSearchByMeals(searchText: String) = viewModelScope.launch {
+        try {
+            val response = repository.remote.getFilterByMeals(searchText)
+            if (response.isSuccessful) {
+                searchByMeals.value = response.body()
+            }
+        } catch (e: Exception) {
+            Log.e("Error Search Meal", e.message.toString())
         }
     }
 
