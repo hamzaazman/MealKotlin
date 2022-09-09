a8b6519cad48264ed4800b0a9fdba31370fc6056
Hamza Azman <hamzaazman@outlook.com>
2022-09-09 16:09:14 +0300

    Rename api service name and add filter functions for fetch api

--- app/src/main/java/com/example/mealkotlin/data/RemoteDataSource.kt
+++ app/src/main/java/com/example/mealkotlin/data/RemoteDataSource.kt
@@ -2,15 +2,23 @@ package com.example.mealkotlin.data
 
 import com.example.mealkotlin.data.network.FoodApi
 import com.example.mealkotlin.models.Categories
+import com.example.mealkotlin.models.Ingredient
 import com.example.mealkotlin.models.Meals
 import retrofit2.Response
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
-    private val categoryApi: FoodApi,
+    private val foodApi: FoodApi,
 ) {
 
-    suspend fun getCategories(): Response<Categories> = categoryApi.getCategories()
+    suspend fun getCategories(): Response<Categories> = foodApi.getCategories()
+
     suspend fun getMealsByCategoryName(categoryName: String): Response<Meals> =
-        categoryApi.getMealsByCategoryName(categoryName)
+        foodApi.getMealsByCategoryName(categoryName)
+
+    suspend fun getIngredientMealByMealId(mealId: Int): Response<Ingredient> =
+        foodApi.getMealIngredientByMealId(mealId)
+
+    suspend fun getFilterByMeals(searchText: String): Response<Meals> =
+        foodApi.getFilterByMeals(searchText)
 }
